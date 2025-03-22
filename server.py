fa0dfafda2bf4b2b3cd5c20a266a07b37f318b77
Đỗ Khắc Quyền <quyendk.b24dtcn380@stu.ptit.edu.vn>
2025-03-22 13:00:39 +0700

    Add files via upload

---
+++ server.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify
+import pytesseract
+from PIL import Image
+import io
+
+app = Flask(__name__)
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'image' not in request.files:
+        return jsonify({"error": "No image uploaded"}), 400
+
+    file = request.files['image']
+    image = Image.open(io.BytesIO(file.read()))
+
+    # Trích xuất văn bản và tọa độ
+    data = pytesseract.image_to_data(image, output_type=pytesseract.Output.DICT)
+
+    results = []
+    for i in range(len(data['text'])):
+        if data['text'][i].strip():
+            results.append({
+                "text": data['text'][i],
+                "x": data['left'][i],
+                "y": data['top'][i],
+                "width": data['width'][i],
+                "height": data['height'][i]
+            })
+
+    return jsonify(results)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
